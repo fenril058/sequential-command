8b55ba9c8cf51524a7d5e940b9ecbf7d71836a8c
ril <fenril.nh@gmail.com>
2022-07-09 19:27:16 +0900

    restore sequentail-command.el sequential-command-config.el

---
+++ sequential-command-config.el
@@ -0,0 +1,38 @@
+;;; seq-command.el --- Many commands into one command -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2009  rubikitch
+
+;; Author: rubikitch <rubikitch@ruby-lang.org>
+;; Maintainer: ril <fenril.nh@gmail.com>
+;; Keywords: convenience, lisp
+;; Version: 1.5.0
+;; URL: https://github.com/fenril058/sequential-command
+;; Package-Requires: (((cl-lib "0.5"))
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;; This file needs to maintaine backward compatibility.
+
+;;; Code:
+
+(require 'seq-command-config)
+
+(defalias 'sequential-command-setup-keys 'seq-command-setup-keys)
+
+(provide 'sequential-command)
+;;; sequential-commands.el ends here
